1eb079ff911d079186503f12520c845545ac933a
kennshi <edb.nikolaevich@gmail.com>
2025-03-09 17:53:08 +0200

    Add MVVM support and data bindings for weather info
    Updated project file to include CommunityToolkit.Mvvm. Created `WeatherInfoPageViewModel` with observable properties for weather data and a command to fetch weather information. Modified `WeatherInfoPage.xaml` to bind UI elements to ViewModel properties, enabling dynamic updates for properties.

---
+++ MauiWeatherApp/Models/ViewModels/WeatherInfoPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiWeatherApp.Models.ViewModels;
+
+internal partial class WeatherInfoPageViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string latitude;
+
+    [ObservableProperty]
+    private string longitude;
+
+    [ObservableProperty]
+    private string weatherIcon;
+
+    [ObservableProperty]
+    private string temperature;
+
+    [ObservableProperty]
+    private string weatherDescription;
+
+    [ObservableProperty]
+    private string location;
+
+    [ObservableProperty]
+    private string humidity;
+
+    [ObservableProperty]
+    private string cloudCoverLevel;
+
+    [ObservableProperty]
+    private string isDay;
+
+    [RelayCommand]
+    private async Task FetchWeatherInformationAsync()
+    {
+
+    }
+}
