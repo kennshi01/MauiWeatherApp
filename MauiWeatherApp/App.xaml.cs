af4f15374cd34b2a33074fec333a31e925a8b395
kennshi <edb.nikolaevich@gmail.com>
2025-03-09 17:41:01 +0200

    Update main page to WeatherInfoPage and add UI to this XAML

--- MauiWeatherApp/App.xaml.cs
+++ MauiWeatherApp/App.xaml.cs
@@ -1,12 +1,13 @@
-﻿namespace MauiWeatherApp
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿using MauiWeatherApp.Pages;
 
-            MainPage = new AppShell();
-        }
+namespace MauiWeatherApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new WeatherInfoPage();
     }
 }
